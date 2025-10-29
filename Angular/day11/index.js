0d4fdb170a8206ac334b2b6dde2eca879c5bccf1
Bimal Devasia <Bimal.devasia@geekywolf.local>
2025-10-29 12:23:37 +0530

    javascript

---
+++ Angular/day11/index.js
@@ -0,0 +1,13 @@
+
+
+function AlertMe() {
+    alert("Button Clicked!");
+}
+
+function checkLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        console.log(year + " is a leap year.");
+    } else {
+        console.log(year + " is not a leap year.");
+    }   
+}
